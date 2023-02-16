745c66d2ff6d9dd17fc49d089b481213878ef8e9
Nefnief-tech <fabian.hirsch@student.evbg.de>
2023-02-16 18:42:08 +0100

    fghb

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
 function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");  
-
-
-
-
-
-
-
-    var x = document.getElementById("myDIV");
-    if (x.innerHTML === "darkmode") {
-      x.innerHTML = "darkmode";
-    } else {
-      x.innerHTML = "lightmode";
-    }
   
 }
 
