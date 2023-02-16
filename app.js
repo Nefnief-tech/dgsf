f79e0e58d818b12ac3e6063de3bdf3094faded80
Nefnief-tech <fabian.hirsch@student.evbg.de>
2023-02-16 17:38:19 +0100

    webside-darkmode-switch-new-release-5.7

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");  
+
+
+
+
+
+
+
+    var x = document.getElementById("myDIV");
+    if (x.innerHTML === "darkmode") {
+      x.innerHTML = "darkmode";
+    } else {
+      x.innerHTML = "lightmode";
+    }
+  
 }
+
