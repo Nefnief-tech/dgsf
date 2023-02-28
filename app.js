ad1de62a0616c33c435c919076da7d72c399821b
Nefnief-tech <fabian.hirsch@student.evbg.de>
2023-02-28 19:42:54 +0100

    2.0.9
    cookie banner

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ window.onload = function() {
       document.body.classList.add('dark-mode');
   }
 }
+    // Check if the cookie has already been set
+    if (document.cookie.indexOf('cookieAccepted=true') === -1) {
+      // If not, display the cookie banner
+      var cookieBanner = document.getElementById('cookie-banner');
+      cookieBanner.style.display = 'block';
+    
+      // When the user clicks the accept button, set the cookie and hide the banner
+      var acceptButton = document.getElementById('accept-cookie');
+      acceptButton.addEventListener('click', function() {
+        document.cookie = 'cookieAccepted=true; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+        cookieBanner.style.display = 'none';
+      });
+    }
